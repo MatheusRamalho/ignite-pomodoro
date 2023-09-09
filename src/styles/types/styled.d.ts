34b2a4848ad41c8c498eae2e45409a59cfbb721d
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2023-09-09 12:09:19 -0300

    feat(main): teste

--- src/styles/types/styled.d.ts
+++ src/styles/types/styled.d.ts
@@ -5,5 +5,6 @@ import { defaultTheme } from '../themes/default'
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface DefaultTheme extends ThemeType {}
 }
